fa2f610bc53465c3802ac539260d0397aff02bfe
Kostyan89 <kostasamin@gmail.com>
2022-02-13 22:08:45 +0200

    add some changes in rendering pages

--- game/app.py
+++ game/app.py
@@ -1,7 +1,22 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+
+from game.equipment import EquipmentData
+from game.personages import personage_classes
+from game.utils import load_equipment
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
+EQUIPMENT: EquipmentData = load_equipment()
+
+
+def render_choose_personage_template(*args, **kwargs) -> str:
+    return render_template(
+            'hero_choosing.html',
+            classes=personage_classes.values(),
+            equipment=EQUIPMENT,
+            **kwargs,
+        )
 
 
 @app.route('/')
@@ -12,7 +27,16 @@ def index():
 @app.route('/choose-hero', methods=['GET', 'POST'])
 def choose_hero():
     if request.method == 'GET':
-        render_template('hero_choosing.html', header='Выберите героя')
-    if request.method == 'POST':
+        return render_choose_personage_template(header='Выберите героя', next_button='Выбрать врага')
+    ...
+    return redirect(url_for('choose_enemy'))
+
+
+@app.route('/choose-enemy', methods=['GET', 'POST'])
+def choose_enemy():
+    if request.method == 'GET':
+        return render_choose_personage_template(header='Выберите врага', next_button='Начать сражение')
+    ...
+    return '<h2>Not Implemented</h2>'
 
 
