68bd9c520230423a309f247b04a0d2dafb25af9b
Kostyan89 <kostasamin@gmail.com>
2022-02-13 21:15:47 +0200

    changed some parameters to float and add classes

--- game/personages.py
+++ game/personages.py
@@ -1,6 +1,6 @@
 from abc import ABC
 
-from game.skills import Skill
+from game.skills import Skill, ferocious_kick, powerful_thrust
 
 
 class Personage(ABC):
@@ -11,3 +11,23 @@ class Personage(ABC):
     attack: float = NotImplemented
     armor: float = NotImplemented
     skill: Skill = NotImplemented
+
+
+class Warrior(Personage):
+    name: str = 'Воин'
+    max_health: float = 60.0
+    max_stamina: float = 30.0
+    stamina: float = 0.8
+    attack: float = 0.9
+    armor: float = 1.2
+    skill: Skill = ferocious_kick
+
+
+class Thief(Personage):
+    name: str = 'Вор'
+    max_health: float = 50.0
+    max_stamina: float = 25.0
+    stamina: float = 1.5
+    attack: float = 1.2
+    armor: float = 1.0
+    skill: Skill = powerful_thrust
